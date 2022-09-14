788b5998d79a1a14a23ff01c13a4c766471793a7
chloebyrnes <107075274+chloebyrnes@users.noreply.github.com>
2022-09-14 18:27:27 -0400

    updated login.js

--- src/js/login.js
+++ src/js/login.js
@@ -2,6 +2,14 @@ const mysql = require('mysql');
 const express = require('express');
 const session = require('express-session');
 const path = require('path');
+
+const connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : '',
+	database : 'nodelogin'
+});
+
 const app = express();
 
 app.use(session({
@@ -13,48 +21,44 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'static')));
 
-
-app.get('http://localhost:3002', function(request, response) {
+app.get('/', function(request, response) {
 	// Render login template
 	response.sendFile(path.join(__dirname + '/login.html'));
 });
 
 app.post('/auth', function(request, response) {
 	// Capture the input fields
-	let email = request.body.email;
+	let username = request.body.username;
 	let password = request.body.password;
 	// Ensure the input fields exists and are not empty
-	if (email && password) {
-		// Execute SQL query that'll select the account from the database based on the specified email and password
-		connection.query('SELECT * FROM accounts WHERE email = ? AND password = ?', [email, password], function(error, results, fields) {
+	if (username && password) {
+		// Execute SQL query that'll select the account from the database based on the specified username and password
+		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
 			// If there is an issue with the query, output the error
 			if (error) throw error;
 			// If the account exists
 			if (results.length > 0) {
 				// Authenticate the user
 				request.session.loggedin = true;
-				request.session.email = email;
-				// Redirect to home page
-				response.redirect('/home');
+				request.session.username = username;
+				// Redirect to search page
+				response.redirect('/search');
 			} else {
-				response.send('Incorrect Email and/or Password!');
+				response.send('Incorrect Username and/or Password!');
 			}			
 			response.end();
 		});
 	} else {
-		response.send('Please enter Email and Password!');
+		response.send('Please enter Username and Password!');
 		response.end();
 	}
 });
 
-//The above code will create our authentication route using the POST method, which will capture our input fields when the user submits the form.
-
-// http://localhost:3002/search
 app.get('/search', function(request, response) {
 	// If the user is loggedin
 	if (request.session.loggedin) {
 		// Output username
-		response.send('Welcome back!');
+		response.send('Welcome back, ' + request.session.username + '!');
 	} else {
 		// Not logged in
 		response.send('Please login to view this page!');
@@ -62,4 +66,4 @@ app.get('/search', function(request, response) {
 	response.end();
 });
 
-app.listen(3002);
+app.listen(3000);
