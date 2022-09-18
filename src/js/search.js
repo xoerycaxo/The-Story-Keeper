66512f224cc95ff19bf84a1e1b27e7fa6989eca4
chloebyrnes <107075274+chloebyrnes@users.noreply.github.com>
2022-09-18 17:10:51 -0400

    added view book button so user can read book if available

--- src/js/search.js
+++ src/js/search.js
@@ -79,8 +79,8 @@ $(document).ready(function() {
      */
      function formatOutput(bookImg, title, author, publisher, bookLink, bookIsbn) {
        // console.log(title + ""+ author +" "+ publisher +" "+ bookLink+" "+ bookImg)
-       var viewUrl = 'mybooks.html?isbn='+bookIsbn; //constructing link for bookviewer
-       var viewBook = 'mybooks.html?isbn='+bookIsbn; //constructing link for bookviewer
+       var viewMyBooks = 'mybooks.html?isbn='+bookIsbn; //constructing link for bookviewer
+       var viewUrl = 'book.html?isbn='+bookIsbn; //constructing link for bookviewer
        var htmlCard = `
        <div class="searcharea-output-section">
          <div class="searchcards" style="">
@@ -93,8 +93,8 @@ $(document).ready(function() {
                  <h5 class="card-title">${title}</h5>
                  <p class="card-text">Author: ${author}</p>
                  <p class="card-text">Publisher: ${publisher}</p><br />
-                 <a href="${viewUrl}" class="addbooksbtn">Add to My Books</a>
-                 <a href="${viewBook}" class="addbooksbtn">Read Book</a>
+                 <a href="${viewMyBooks}" class="addbooksbtn">Add to My Books</a><br /><br />
+                 <a target="_blank" href="${viewUrl}" class="addbooksbtn">View Book</a>
                </div>
              </div>
            </div>
