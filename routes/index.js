7d51234322cda3afa58f7b6669f142027afc281c
nashalysf <nashalysf@gmail.com>
2022-09-21 20:38:13 -0400

    css update

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const apiRoutes = require("./api");
 const viewRoutes = require("./htmlroutes");
 
-router.use('/', apiRoutes);
+router.use('/api', apiRoutes);
 
 module.exports = router;
